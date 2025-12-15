4b94dde7cf989c590a05bc90a62304d31f6480e6
longxiangyu <zerolxy612@gmail.com>
2025-12-15 17:27:26 +0800

    inti project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './index.css';
+
+function App() {
+  return (
+    <main>
+      <h1>Finchat React + Vite</h1>
+      <p>工程已完成基础搭建，后续可在此处开始开发页面。</p>
+    </main>
+  );
+}
+
+export default App;
